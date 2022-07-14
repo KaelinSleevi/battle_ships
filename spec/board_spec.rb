b32a071d5c823e50741d1db6b2f5ef09d7bb984b
Dominic ODonnell <dominicodonnell99@gmail.com>
2022-07-13 19:53:59 -0500

    feat: create new tests for valid_placement?

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,5 +1,6 @@
 require './lib/board'
 require './lib/cell'
+require './lib/ship'
 require 'rspec'
 
 describe Board do
@@ -36,6 +37,26 @@ describe Board do
     expect(board.valid_coordinate?('A5')).to eq(false)
     expect(board.valid_coordinate?('B5')).to eq(false)
     expect(board.valid_coordinate?('C5')).to eq(false)
+  end
+
+  it 'valid_placement? returns false if incorrect ship length placed' do
+    board = Board.new
+
+    cruiser = Ship.new("Crusier", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(crusier, ["A1", "A2"])).to eq(false)
+    expect(board.valid_placement?(submarine, ["A2", "A3", "A4"])).to eq(false)
+  end
+
+  it 'valid_placement? returns true if correct ship length placed' do
+    board = Board.new
+
+    cruiser = Ship.new("Crusier", 3)
+    submarine = Ship.new("Submarine", 2)
+
+    expect(board.valid_placement?(crusier, ["A2", "A3", "A4"])).to eq(true)
+    expect(board.valid_placement?(submarine, ["A2", "A3"])).to eq(true)
   end
 
 end
