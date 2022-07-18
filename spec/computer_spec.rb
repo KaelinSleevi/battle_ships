c44dbf2d22f61415be3f3585a4c071f327775c7a
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-07-18 16:07:51 -0600

    add a test file for the computer class

--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -5,12 +5,23 @@ require './lib/game'
 require './lib/computer'
 require 'rspec'
 
-RSpec.describe Cell do
+RSpec.describe Computer do
     before(:each) do
-      @cell = Cell.new('B4')
+      @computer = Computer.new
     end
-  
+
     it 'exists' do
-      expect(@cell).to be_an_instance_of(Cell)
+      expect(@computer).to be_an_instance_of(Computer)
     end
-  end
+
+    # it 'can place ships' do
+    #
+    #
+    #
+    #   expect(@computer.place_ships).to eq()
+    # end
+    #
+    # it 'can move' do
+    #   expect(@computer.move).to eq()
+    # end
+end
